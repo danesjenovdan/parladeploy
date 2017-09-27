f4972e116fe595bbabad3d02ac7a4217044e7817
FranciZ <franci@proxima.si>
2017-09-27 17:37:23 +0200

    Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -40,7 +40,7 @@ module.exports = {
       ref           : 'origin/master',
       repo          : 'git@parladeploy:danesjenovdan/parladeploy.git',
       path          : '/home/parladaddy/parladeploy',
-      //'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env staging'
+      'post-deploy' : 'npm install'
     },
     dev        : {
       user          : 'node',
