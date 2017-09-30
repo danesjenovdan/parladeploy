543c01ac6cfeb0d71c0f18912c969de51d7edaaf
FranciZ <franci@proxima.si>
2017-09-30 16:05:07 +0200

    Update

--- config.staging.js
+++ config.staging.js
@@ -2,6 +2,6 @@ module.exports = {
 
   PORT              : 3051,
   PROJECTS_DIR_PATH : '/home/parladaddy',
-  DEPLOY_SCRIPT_PATH: '/home/centos/parlameter/deploy'
+  DEPLOY_SCRIPT_PATH: '/home/parladaddy/parladeploy/current'
 
 };
