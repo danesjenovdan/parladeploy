5af049d65523c42741a79961fcab43257a4af94d
FranciZ <franci@proxima.si>
2017-10-01 16:12:39 +0200

    Update output url

--- config.prod.js
+++ config.prod.js
@@ -3,6 +3,6 @@ module.exports = {
   PORT               : 7007,
   PROJECTS_DIR_PATH  : '/home/centos/parlameter',
   DEPLOY_SCRIPT_PATH : '/home/centos/parlameter/deploy/current',
-  OUTPUT_URL         : 'https://hooks.slack.com/services/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'
+  OUTPUT_URL         : 'https://hooks.slack.com/services/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'
 
 };
--- config.staging.js
+++ config.staging.js
@@ -3,6 +3,6 @@ module.exports = {
   PORT               : 3051,
   PROJECTS_DIR_PATH  : '/home/parladaddy',
   DEPLOY_SCRIPT_PATH : '/home/parladaddy/parladeploy/current',
-  OUTPUT_URL         : 'https://hooks.slack.com/services/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'
+  OUTPUT_URL         : 'https://hooks.slack.com/services/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'
 
 };
