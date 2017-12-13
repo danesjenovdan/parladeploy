36e90187703d679e1d2a005289e1144a8e6795a8
Filip Muki Dobranic <filipdobranic@gmail.com>
2017-12-13 19:16:29 +0100

    still fixing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app        = express();
 const config     = require('./config');
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({ extended : true }));
 
 exports.init = () => {
