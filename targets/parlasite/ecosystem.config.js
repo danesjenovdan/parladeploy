3a804ac16b26956ac3b9d64c2a1ce0b975c6b17f
Parla Daddy <filipdobranic@gmail.com>
2017-12-13 15:38:21 +0000

    use yarn and other fixes

--- targets/parlasite/ecosystem.config.js
+++ targets/parlasite/ecosystem.config.js
@@ -47,7 +47,7 @@ module.exports = {
       ref           : 'origin/staging',
       repo          : 'git@parlasite:danesjenovdan/parlasite.git',
       path          : `${config.PROJECTS_DIR_PATH}/parlasite`,
-      'post-deploy' : `npm install && npm update && MONGO_USERNAME=\${MONGO_USERNAME} MONGO_PASSWORD=\${MONGO_PASSWORD} pm2 startOrRestart ${config.DEPLOY_SCRIPT_PATH}/targets/parlasite/ecosystem.config.js --env staging --update-env`,
+      'post-deploy' : `yarn && MONGO_USERNAME=\${MONGO_USERNAME} MONGO_PASSWORD=\${MONGO_PASSWORD} pm2 startOrRestart ${config.DEPLOY_SCRIPT_PATH}/targets/parlasite/ecosystem.config.js --env staging --update-env`,
       env           : {
         NODE_ENV       : 'staging',
       }
