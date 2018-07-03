d8a3fe3e1476281c5540cac333355d1ca6e665c7
LepkoQQ <lepko.san@gmail.com>
2018-07-03 18:35:24 +0200

    add cross-env

--- config.prod.js
+++ config.prod.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: 7007,
   PROJECTS_DIR_PATH: '/home/parlauser',
   DEPLOY_SCRIPT_PATH: '/home/parlauser/parladeploy',
-  OUTPUT_URL: 'https://hooks.slack.com/services/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'
+  OUTPUT_URL: 'https://hooks.slack.com/services/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',
 };
